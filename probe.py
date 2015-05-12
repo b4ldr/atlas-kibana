1340be58c7ecdc8ad81e1f68a75b0ccd5b71f637
John Bond <github@johnbond.org>
2015-05-12 14:27:50 +0200

    add initial commit

---
+++ probe.py
@@ -0,0 +1,191 @@
+import Queue
+import pickle
+import netaddr
+import logging
+import requests
+import ripestat
+import threading
+
+class Probes(object):
+    '''return a class with all atlas probes'''
+    probe_api_url     = 'https://atlas.ripe.net/api/v1/probe/'
+    probe_archive_url = 'https://atlas.ripe.net/api/v1/probe-archive/?format=json'
+
+    def __init__(self, refresh=False, probes_file='probes.p', threads=200):
+        '''initialise class'''
+        self.probes_file = probes_file
+        self.logger      = logging.getLogger('atlas-kibana.Probes')
+        self.threads     = threads
+        self.queue       = Queue.Queue(self.threads * 2)
+        self.probes      = set()
+        self.load()
+        if refresh:
+            self.refresh()
+        self.save()
+
+    def exists(self, probe_id):
+        '''check if we already have this probe'''
+        for probe in self.probes.copy():
+            if probe.id == probe_id:
+                return True
+        return False
+
+    def get(self, probe_id):
+        '''get an probe element'''
+        for probe in self.probes.copy():
+            if probe.id == probe_id:
+                return probe
+        return False
+
+    def load(self):
+        '''load pickle data'''
+        try:
+            self.probes = pickle.load(open(self.probes_file, 'rb'))
+        except IOError:
+            self.logger.warning('unable to load probes file {}'.format(self.probes_file))
+
+    def load_probe(self):
+        '''load a probe from the queue'''
+        while True:
+            probe = self.queue.get()
+            try:
+                if not self.exists(probe['id']):
+                    self.probes.add(Probe(probe))
+                    self.logger.info('{}:Added'.format(probe['id']))
+                else:
+                    self.logger.debug('{}:Already Exists'.format(probe['id']))
+            except Exception as e: 
+                # This will likly come back and bite me :S
+                self.logger.warning('{}:could not process\n{}'.format(probe['id'], e))
+            self.queue.task_done()
+
+    def refresh(self):
+        '''load all atlas probes'''
+        try:
+            self.logger.debug('fetching {}'.format(self.probe_archive_url))
+            probes = requests.get(self.probe_archive_url, verify=False).json().get(
+                    'objects', [])
+            self.logger.debug('fetched {}'.format(self.probe_archive_url))
+        except Exception as e:
+            self.logger.error('Could not fetch {0}:\n{1}'.format(self.probe_archive_url, e))
+            return False
+        for i in range(self.threads):
+            thread        = threading.Thread(target=self.load_probe)
+            thread.daemon = True
+            thread.start()
+        for probe in probes:
+            self.queue.put(probe)
+        self.queue.join()
+        return True
+
+    def save(self):
+        '''save the probes data'''
+        pickle.dump(self.probes, open(self.probes_file, 'wb'))
+
+class Probe(object):
+
+    def __init__(self, probe):
+        self.logger       = logging.getLogger('atlas-kibana.Probe')
+        self.staRuntimeErrortus       = probe.get('status_name',  None)
+        self.status_since = probe.get('status_since', None)
+        self.address_v4   = probe.get('address_v4',   None)
+        self.address_v6   = probe.get('address_v6',   None)
+        self.asn_v4       = probe.get('asn_v4',       None)
+        self.asn_v6       = probe.get('asn_v6',       None)
+        self.country_code = probe.get('country_code', None)
+        self.latitude     = probe.get('latitude',     None)
+        self.longitude    = probe.get('longitude',    None)
+        self.prefix_v4    = probe.get('prefix_v4',    None)
+        self.prefix_v6    = probe.get('prefix_v6',    None)
+        self.id           = probe.get('id',           None)
+        self.is_anchor    = probe.get('is_anchor',    None)
+        self.is_public    = probe.get('is_public',    None)
+        self.resource_uri = probe.get('resource_uri', None)
+        self.tooltip      = 'Probe #{0}: https://atlas.ripe.net/probes/{0}/'.format(self.id)
+        self.geojson      = [self.longitude, self.latitude]
+        self.stat_api     = ripestat.StatAPI('Atlas-Kibana')
+        self.location     = self.get_location(self.country_code)
+        self.tags         = []
+        self.tags.extend(probe.get('tags', None))
+        self.asn_v4_name  = None
+        self.asn_v6_name  = None
+        self.prefixlen_v4 = None
+        self.rir_v4       = None
+        self.prefixlen_v6 = None
+        self.rir_v6       = None
+
+        if self.asn_v4:
+            self.asn_v4_name  = self.get_asn_name(self.asn_v4)
+        if self.asn_v6:
+            if self.asn_v6 == self.asn_v4:
+                self.asn_v6_name  = self.asn_v4_name
+            else:
+                self.asn_v6_name  = self.get_asn_name(self.asn_v6)
+        if self.prefix_v4:
+            self.prefixlen_v4 = self.get_prefix_len(self.prefix_v4)
+            self.rir_v4       = self.get_rir(self.prefix_v4)
+        if self.prefix_v6:
+            self.prefixlen_v6 = self.get_prefix_len(self.prefix_v6)
+            self.rir_v6       = self.get_rir(self.prefix_v6)
+
+    def __eq__(self, other):
+        '''Check equality'''
+        if isinstance(other, Probe):
+            return self.id == other.id
+        return False
+
+    def __ne__(self, other):
+        '''Check inequality'''
+        if isinstance(other, Probe):
+            return not (self.id == other.id)
+        return True
+
+    def __repr__(self):
+        '''represent object'''
+        return 'Probe({})'.format(self.id)
+
+    def __hash__(self):
+        '''hash object'''
+        return hash(self.__repr__())
+    
+
+
+    def get_rir(self, prefix):
+        '''use RIPEstat to get the rir name'''
+        try:
+            whois = self.stat_api.get_data('whois', {'resource': prefix})
+            rir   = ','.join(whois.get('authorities', []))
+            self.logger.debug('{}:Add rir {} for {}'.format(self.id, rir, prefix))
+            return rir
+        except Exception as e:
+            self.logger.error('Could not fetch holder for {}:\n{}'.format(prefix, e))
+
+    def get_asn_name(self, asn):
+        '''use RIPEstat to get the asn name'''
+        try:
+            asn_name = self.stat_api.get_data('as-overview',
+                {'resource': asn}).get('holder', None)
+            self.logger.debug('{}:Add asn name {} for {}'.format(
+                self.id, asn_name, asn))
+            return asn_name
+        except Exception as e:
+            self.logger.error('Could not fetch holder for {}:\n{}'.format(asn, e))
+        
+    def get_location(self, country_code):
+        '''use restcountries.eu to get the location'''
+        url = 'https://restcountries.eu/rest/v1/alpha?codes={}'.format(country_code.lower())
+        try:
+            location = requests.get(url, verify=False).json()
+            self.logger.debug('{}:Add location for {}'.format(self.id, country_code))
+            return location
+        except Exception as e:
+            self.logger.error('Could not fetch {}:\n{}'.format(url, e))
+
+    def get_prefix_len(self, prefix):
+        try:
+            prefix_len = int(netaddr.IPNetwork(prefix).prefixlen)
+            self.logger.debug('{}:Add prefix length {} for {}'.format(
+                self.id, prefix_len, prefix))
+            return prefix_len
+        except netaddr.AddrFormatError as e:
+            self.logger.warning('could not get prefix length: {}'.format(e))
---
+++ ripestat.py
@@ -0,0 +1,226 @@
+from cookielib import CookieJar, Cookie
+import sys
+import urllib
+import urllib2
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+__version__ = '0.23'
+
+'''
+This code is copied from the in the following project
+https://github.com/RIPE-NCC/ripestat-text
+and is covered by the GNU v3 licence
+https://github.com/RIPE-NCC/ripestat-text/blob/master/LICENCE
+'''
+
+class StatAPI(object):
+    """
+    A Python wrapper around the RIPEstat Data API.
+
+    Usage:
+        from ripestat.api import StatAPI
+        api = StatAPI("My Client Name")
+        api.get_data(<resourcename>, {"resource": <someresource>})
+
+    Example (Basic request):
+        api.get_data("geoloc", {"resource": "193.0.6.139/24"})
+
+    Example (Fetch the meta data):
+        api.get_data("geoloc", {"resource": "193.0.6.139/24"}).meta
+
+    """
+
+    RIPE_ACCESS = "https://access.ripe.net"
+    DATA_API = "https://stat.ripe.net/data/"
+
+    class Error(Exception):
+        """
+        The base class for exceptions raised by this class.
+        """
+
+    class ServerError(Error):
+        """
+        Raised when an unsuccesful response is received from the server.
+        """
+        def __init__(self, http_error):
+            self.response = json.loads(http_error.read())
+            self.status_code = http_error.code
+            errors = [m[1] for m in self.response["messages"] if m[0] ==
+                "error"]
+            super(StatAPI.ServerError, self).__init__(*errors)
+
+    class VersionError(Error):
+        """
+        Raised when there is a mismatch between expected and actual version
+        numbers.
+        """
+        def __init__(self, call, requested, actual):
+            StatAPI.Error.__init__(self, "expected version {1}.x of the '{0}' "
+                "data call; found {2}".format(call, requested, actual))
+
+    def __init__(self, caller_id, base_url=DATA_API, headers=None, token=None):
+        self.base_url = base_url
+
+        self.cookiejar = StatCookieJar(token)
+
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(
+            self.cookiejar))
+
+        # These are the parts of the User-Agent that stay constant
+        self.ua_parts = [
+            "ripestat-text/%s" % __version__,
+            "python/" + ".".join(str(v) for v in sys.version_info[:3]),
+            "platform/" + sys.platform
+        ]
+        # The caller_id is added to the User-Agent header.
+        # It can be changed after instantiation.
+        self.caller_id = caller_id
+
+        self.headers = headers
+
+    def get_session(self):
+        """
+        Carry out a single request in order to get a session cookie.
+        """
+        if self.cookiejar.token:
+            return self.cookiejar.token
+        self.get_response()
+        return self.cookiejar.token
+
+    def get_data(self, call, query=None, version=None):
+        """
+        Execute and deserialize a single RIPEstat data call, possibly
+        requesting a specific version.
+        """
+        json_response = self.get_response("%s/data.json" % call, query)
+        response = json.loads(json_response)
+        if version is not None:
+            maj_version, min_version = response["version"].split(".", 2)
+            if int(maj_version) != version:
+                raise self.VersionError(call, version, response["version"])
+        return DataResponse(response)
+
+    def get_response(self, url=None, query=None):
+        """
+        Return the (serialized) body of a raw data response.
+        """
+        if url:
+            url = "%s/%s" % (self.base_url.rstrip("/"), url)
+        else:
+            url = self.base_url
+        if not url.startswith("http"):
+            url = "https://" + url
+        if query:
+            url += "?" + urllib.urlencode(query)
+
+        try:
+            response = self.open(url).read()
+        except urllib2.HTTPError as exc:
+            raise self.ServerError(exc)
+        return response.decode("UTF-8")
+
+    def open(self, url, *args, **kwargs):
+        """
+        Wrapper around the urllib2 opener that sets a User-Agent header
+        based on the calling application, the running mode
+        """
+        if isinstance(url, basestring):
+            url = urllib2.Request(url)
+        ua_parts = self.ua_parts
+        if self.caller_id:
+            ua_parts = [self.caller_id] + ua_parts
+        url.add_header("User-agent", " ".join(ua_parts))
+        if self.headers:
+            for header in self.headers:
+                url.add_header(*header)
+        return self.opener.open(url, *args, **kwargs)
+
+
+    def login(self, username, password):
+        """
+        Authenticate with the RIPE NCC Access framework by asking for a
+        password from the user.
+        """
+        response = self.open(self.RIPE_ACCESS, urllib.urlencode({
+                "username": username,
+                "password": password,
+                "originalUrl": "",
+            }))
+        return "Welcome," in response.read()
+
+
+class StatCookieJar(CookieJar):
+    """
+    CookieJar that remembers and reinserts RIPE NCC Access cookies.
+    """
+    CROWD_COOKIE = "crowd.token_key"
+    STAT_COOKIE = "stat-session"
+
+    def __init__(self, token=None):
+        CookieJar.__init__(self)
+
+        if token:
+            parts = token.split("_")
+            if len(parts) == 2:
+                crowd, stat = parts
+                self.make_cookie(self.CROWD_COOKIE, crowd)
+                self.make_cookie(self.STAT_COOKIE, stat)
+
+    def make_cookie(self, name, value):
+        """
+        Create and set a cookie with the given name and value.
+        """
+        cookie = Cookie(
+            name=name,
+            value=value,
+            domain='.ripe.net',
+            path='/',
+
+            version=0,
+            port=None,
+            port_specified=False,
+            domain_specified=False,
+            domain_initial_dot=False,
+            path_specified=None,
+            secure=True,
+            expires=None,
+            discard=None,
+            comment=None,
+            comment_url=None,
+            rest=None,
+        )
+        self.set_cookie(cookie)
+
+    @property
+    def token(self):
+        """
+        Generate a serialized token suitable for setting in the TOKEN variable.
+        """
+        session = ""
+        token = ""
+        for cookie in self:
+            if cookie.name == self.CROWD_COOKIE:
+                token = cookie.value
+            elif cookie.name == self.STAT_COOKIE:
+                session = cookie.value
+        if token and session:
+            return token + "_" + session
+        else:
+            return ""
+
+
+class DataResponse(dict):
+    """
+    The response from a data API call.
+
+    It works like the "data" portion of the data response, but the remaining
+    metadata is available as `response.meta`.
+    """
+    def __init__(self, response):
+        dict.__init__(self)
+        self.update(response["data"])
+        del response["data"]
+        self.meta = response
